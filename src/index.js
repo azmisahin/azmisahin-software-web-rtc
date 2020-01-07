c21db499a1f80d0e15e1c44aabdb06779eda4c90
Azmi ŞAHİN <azmisahin@outlook.com>
2020-01-07 20:41:28 +0300

    Socket.io Listener

--- src/index.js
+++ src/index.js
@@ -5,16 +5,33 @@ var static = require('node-static');
 
 // Root
 var file = new static.Server('./');
- 
-// Http Request and Response
-require('http').createServer(function (request, response) {
+
+// Request Listener
+function requestListener(request, response) {
+
+    // Request Listener end
     request.addListener('end', function () {
-        //
-        // Serve files!
-        //
+
+        // Static File Serve
         file.serve(request, response);
     }).resume();
+}
 
-})
-// Listen Port
-.listen(8080);
+// Http Server
+const server = require('http').createServer(requestListener);
+
+// Websocket
+const io = require('socket.io')(server);
+
+// Socket Connection
+io.on('connection', client => {
+
+    // Data
+    client.on('data', data => {
+        //
+        client.broadcast.emit('data', data)
+    });
+});
+
+// Http Server Listener
+server.listen(8080);
